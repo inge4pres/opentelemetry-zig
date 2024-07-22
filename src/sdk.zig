c2c6846cffe82e7046d88bd715ac719dc75d8706
inge4pres <fgualazzi@gmail.com>
2024-07-22 22:47:49 +0200

    improve testing of spec helper
    Signed-off-by: inge4pres <fgualazzi@gmail.com>

--- src/sdk.zig
+++ src/sdk.zig
@@ -1,7 +1,9 @@
 // OpenTelemetry SDK implementation for Zig.
 
 test {
+    // SDK implementations
     _ = @import("metrics_test.zig");
+    // helpers
     _ = @import("pb_utils.zig");
+    _ = @import("spec.zig");
 }
-
