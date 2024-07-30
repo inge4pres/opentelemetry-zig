f7557d5e1c0ae25cab87670b1f68620e92853ad8
inge4pres <fgualazzi@gmail.com>
2024-07-30 11:42:39 +0200

    histogram implementation
    Signed-off-by: inge4pres <fgualazzi@gmail.com>

--- src/metrics.zig
+++ src/metrics.zig
@@ -116,12 +116,35 @@ const Meter = struct {
         });
         return counter;
     }
+
+    pub fn createHistogram(self: *Self, comptime T: type, options: InstrumentOptions) !Histogram(T) {
+        switch (T) {
+            isize, i8, i16, i32, i64, f32, f64 => {},
+            else => @compileError("Unsupported histogram value type"),
+        }
+
+        const histogram = try Histogram(T).init(options, self.allocator);
+        // FIXME double registration?
+        try self.instruments.put(options.name, pb_metrics.Metric{
+            .name = .{ .Const = options.name },
+            .unit = if (options.unit) |u| .{ .Const = u } else .Empty,
+            .description = if (options.description) |d| .{ .Const = d } else .Empty,
+            .data = .{ .histogram = histogram.measures },
+            // These metadata are optional and can be used to add attributes to describe the metric.
+            .metadata = std.ArrayList(pb_common.KeyValue).init(self.allocator),
+        });
+        return histogram;
+    }
 };
 
 pub const InstrumentOptions = struct {
     name: []const u8,
     description: ?[]const u8 = null,
     unit: ?[]const u8 = null,
+    /// ExplcitBuckets is used only in histograms to specify the bucket boundaries.
+    /// Leave empty for default SDK buckets.
+    explicitBuckets: ?[]const f64 = null,
+    recordMinMax: bool = true,
     // Advisory parameters are in development, we don't support them here.
 };
 
@@ -165,6 +188,109 @@ fn Counter(comptime valueType: type) type {
 
         pub fn series(self: *Self) std.AutoHashMap(u64, valueType) {
             return self.cumulative;
+        }
+    };
+}
+
+fn Histogram(comptime valueType: type) type {
+    return struct {
+        const Self = @This();
+        // Define a maximum number of buckets that can be used to record measurements.
+        const maxBuckets = 1024;
+
+        measures: pb_metrics.Histogram,
+        options: InstrumentOptions,
+        // We should keep track of the current value of the counter for each unique comibination of attribute.
+        // At the same time, we don't want to allocate memory for each attribute set that comes in.
+        // So we store all the counters in a single array and keep track of the state of each counter.
+        cumulative: std.AutoHashMap(u64, valueType),
+        // Holds the counts of the values falling in each bucket for the histogram.
+        // The buckets are defined by the user if explcitily provided, otherwise the default SDK specification
+        // buckets are used.
+        // Buckets are always defined as f64.
+        buckets: []const f64,
+        bucket_counts: std.AutoHashMap(u64, []usize),
+        min: ?valueType = null,
+        max: ?valueType = null,
+
+        fn init(options: InstrumentOptions, allocator: std.mem.Allocator) !Self {
+            // Validate name, unit anddescription, optionally throw an error if non conformant.
+            // See https://opentelemetry.io/docs/specs/otel/metrics/api/#instrument-name-syntax
+            try spec.validateInstrumentOptions(options);
+            const buckets = options.explicitBuckets orelse spec.defaultHistogramBucketBoundaries;
+            try spec.validateExplicitBuckets(buckets);
+
+            return Self{
+                .options = options,
+                .measures = pb_metrics.Histogram{
+                    .data_points = std.ArrayList(pb_metrics.HistogramDataPoint).init(allocator),
+                    .aggregation_temporality = pb_metrics.AggregationTemporality.AGGREGATION_TEMPORALITY_CUMULATIVE,
+                },
+                .cumulative = std.AutoHashMap(u64, valueType).init(allocator),
+                .buckets = buckets,
+                .bucket_counts = std.AutoHashMap(u64, []usize).init(allocator),
+            };
+        }
+
+        /// Add the given value to the histogram, using the provided attributes.
+        pub fn record(self: *Self, value: valueType, attributes: ?pb_common.KeyValueList) !void {
+            const key = pbutils.hashIdentifyAttributes(attributes);
+            if (self.cumulative.getEntry(key)) |c| {
+                c.value_ptr.* += value;
+            } else {
+                try self.cumulative.put(key, value);
+            }
+            // Find the value for the bucket that the value falls in.
+            // If the value is greater than the last bucket, it goes in the last bucket.
+            // If the value is less than the first bucket, it goes in the first bucket.
+            // Otherwise, it goes in the bucket for which the boundary is greater than or equal the value.
+            const bucketIdx = self.findBucket(value);
+            if (self.bucket_counts.getEntry(key)) |bc| {
+                bc.value_ptr.*[bucketIdx] += 1;
+            } else {
+                var counts = [_]usize{0} ** maxBuckets;
+                counts[bucketIdx] = 1;
+                try self.bucket_counts.put(key, counts[0..self.buckets.len]);
+            }
+
+            // Update Min and Max values.
+            if (self.options.recordMinMax) {
+                if (self.max) |max| {
+                    if (value > max) {
+                        self.max = value;
+                    }
+                } else {
+                    self.max = value;
+                }
+                if (self.min) |min| {
+                    if (value < min) {
+                        self.min = value;
+                    }
+                } else {
+                    self.min = value;
+                }
+            }
+        }
+
+        fn findBucket(self: Self, value: valueType) usize {
+            const vf64: f64 = @as(f64, @floatFromInt(value));
+            for (self.buckets, 0..) |b, i| {
+                if (b >= vf64) {
+                    return i;
+                }
+            }
+            // The last bucket is returned if the value is greater than it.
+            return self.buckets.len - 1;
+        }
+
+        pub fn series(self: Self) std.AutoHashMap(u64, valueType) {
+            return self.cumulative;
+        }
+        pub fn minAndMax(self: Self) std.meta.Tuple(&.{ valueType, valueType }) {
+            return .{ self.min orelse 0, self.max orelse 0 };
+        }
+        pub fn bucketCounts(self: Self, attributes: ?pb_common.KeyValueList) ?[]usize {
+            return self.bucket_counts.get(pbutils.hashIdentifyAttributes(attributes));
         }
     };
 }
