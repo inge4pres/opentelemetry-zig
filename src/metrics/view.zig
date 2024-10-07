7f2ba5fa12148d46ad97b9ffadee551b0f342455
inge4pres <fgualazzi@gmail.com>
2024-10-08 00:01:11 +0200

    complete histogram exporting
    Signed-off-by: inge4pres <fgualazzi@gmail.com>

--- src/metrics/view.zig
+++ src/metrics/view.zig
@@ -1,3 +1,4 @@
+const pbmetrics = @import("../opentelemetry/proto/metrics/v1.pb.zig");
 const Instrument = @import("instrument.zig");
 
 /// Defines the ways and means to compute aggregated metrics.
@@ -24,6 +25,13 @@ pub fn DefaultAggregationFor(kind: Instrument.Kind) Aggregation {
 pub const Temporality = enum {
     Cumulative,
     Delta,
+
+    pub fn toProto(self: Temporality) pbmetrics.AggregationTemporality {
+        return switch (self) {
+            .Cumulative => .AGGREGATION_TEMPORALITY_CUMULATIVE,
+            .Delta => .AGGREGATION_TEMPORALITY_DELTA,
+        };
+    }
 };
 
 pub fn DefaultTemporalityFor(kind: Instrument.Kind) Temporality {
