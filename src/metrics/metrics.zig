03467b4caa581f4d017c3b90d9417a71a24b6cfb
inge4pres <fgualazzi@gmail.com>
2024-10-28 20:30:30 +0100

    fix exporter and periodic reporter
    Signed-off-by: inge4pres <fgualazzi@gmail.com>

--- src/metrics/metrics.zig
+++ src/metrics/metrics.zig
@@ -2,6 +2,7 @@ test {
     _ = @import("meter.zig");
     _ = @import("instrument.zig");
     _ = @import("spec.zig");
+    _ = @import("attributes.zig");
 }
 
 pub const MeterProvider = @import("meter.zig").MeterProvider;
