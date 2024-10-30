db86d6a68f8c966233de89e6eba627f875967045
inge4pres <fgualazzi@gmail.com>
2024-10-30 22:26:59 +0100

    counter: improve add() API using owned attributes
    Signed-off-by: inge4pres <fgualazzi@gmail.com>

--- src/metrics/measurement.zig
+++ src/metrics/measurement.zig
@@ -4,25 +4,21 @@ const Attribute = a.Attribute;
 const Attributes = a.Attributes;
 
 /// A measurement is a value recorded with an optional set of attributes.
-/// It represents a single data point collected by an instrument.
+/// It represents a single data point collected from an instrument.
 pub fn Measurement(comptime T: type) type {
     return struct {
         const Self = @This();
 
         value: T,
-        attributes: ?[]Attribute,
-
-        pub fn with(value: T, attributes: ?[]Attribute) Measurement(T) {
-            return Self{
-                .value = value,
-                .attributes = attributes,
-            };
-        }
+        attributes: ?[]Attribute = null,
     };
 }
 
-// test "measurement with attributes" {
-//     const attrs = try Attributes.from(std.testing.allocator, .{ "name", 24 });
-//     const m = Measurement(u32).with(42, attrs);
-//     std.testing.expect(m.value == 42);
-// }
+test "measurement with attributes" {
+    const key = "name";
+    const attrs = try Attributes.from(std.testing.allocator, .{ key, true });
+    defer std.testing.allocator.free(attrs.?);
+
+    const m = Measurement(u32){ .value = 42, .attributes = attrs };
+    try std.testing.expect(m.value == 42);
+}
--- src/metrics/metrics.zig
+++ src/metrics/metrics.zig
@@ -3,6 +3,8 @@ test {
     _ = @import("instrument.zig");
     _ = @import("spec.zig");
     _ = @import("attributes.zig");
+    _ = @import("measurement.zig");
+
 }
 
 pub const MeterProvider = @import("meter.zig").MeterProvider;
