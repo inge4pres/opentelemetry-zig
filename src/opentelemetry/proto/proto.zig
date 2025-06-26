567ec8e308803fa2c777a3e689f35453fce8ebb5
inge4pres <fgualazzi@gmail.com>
2025-06-26 22:03:12 +0200

    protobuf: introduce module to export symbols
    Signed-off-by: inge4pres <fgualazzi@gmail.com>

---
+++ src/opentelemetry/proto/proto.zig
@@ -0,0 +1,10 @@
+pub const common = @import("common/v1.pb.zig");
+pub const resource = @import("resource/v1.pb.zig");
+
+pub const metrics = @import("metrics/v1.pb.zig");
+pub const trace = @import("trace/v1.pb.zig");
+pub const logs = @import("logs/v1.pb.zig");
+
+pub const collector_metrics = @import("collector/metrics/v1.pb.zig");
+pub const collector_trace = @import("collector/trace/v1.pb.zig");
+pub const collector_logs = @import("collector/logs/v1.pb.zig");
