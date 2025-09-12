56eb40a926d98a645fb0fa5449f40cb8c0f9a580
inge4pres <fgualazzi@gmail.com>
2025-09-12 23:30:41 +0200

    build: export namespaces by signal
    Signed-off-by: inge4pres <fgualazzi@gmail.com>

--- src/sdk.zig
+++ src/sdk.zig
@@ -18,25 +18,15 @@ test {
 // Export the entire API module for easy access
 pub const api = @import("api.zig");
 
-// SDK exports
+// SDK namespaces
 pub const trace = @import("sdk/trace.zig");
+pub const metrics = @import("sdk/metrics.zig");
+pub const logs = @import("sdk/logs.zig");
 
 // Direct exports for convenience
-pub const MeterProvider = @import("api/metrics/meter.zig").MeterProvider;
-pub const MetricReader = @import("sdk/metrics/reader.zig").MetricReader;
-pub const MetricExporter = @import("sdk/metrics/exporter.zig").MetricExporter;
-pub const InMemoryExporter = @import("sdk/metrics/exporters/in_memory.zig").InMemoryExporter;
-pub const StdoutExporter = @import("sdk/metrics/exporters/stdout.zig").StdoutExporter;
-pub const OTLPExporter = @import("sdk/metrics/exporters/otlp.zig").OTLPExporter;
 pub const otlp = @import("otlp.zig");
 
 // Attribute system exports
 pub const Attribute = @import("attributes.zig").Attribute;
 pub const AttributeValue = @import("attributes.zig").AttributeValue;
 pub const Attributes = @import("attributes.zig").Attributes;
-
-pub const Counter = @import("api/metrics/instrument.zig").Counter;
-pub const UpDownCounter = @import("api/metrics/instrument.zig").Counter;
-pub const Histogram = @import("api/metrics/instrument.zig").Histogram;
-pub const Gauge = @import("api/metrics/instrument.zig").Gauge;
-pub const View = @import("sdk/metrics/view.zig");
--- src/sdk/metrics.zig
+++ src/sdk/metrics.zig
@@ -5,3 +5,18 @@ test {
     _ = @import("metrics/temporality.zig");
     _ = @import("metrics/aggregation.zig");
 }
+
+pub const MeterProvider = @import("../api/metrics/meter.zig").MeterProvider;
+pub const Kind = @import("../api/metrics/instrument.zig").Kind;
+pub const MetricReader = @import("metrics/reader.zig").MetricReader;
+pub const MetricExporter = @import("metrics/exporter.zig").MetricExporter;
+
+pub const Counter = @import("../api/metrics/instrument.zig").Counter;
+pub const UpDownCounter = @import("../api/metrics/instrument.zig").Counter;
+pub const Histogram = @import("../api/metrics/instrument.zig").Histogram;
+pub const Gauge = @import("../api/metrics/instrument.zig").Gauge;
+pub const View = @import("metrics/view.zig");
+
+pub const InMemoryExporter = @import("metrics/exporters/in_memory.zig").InMemoryExporter;
+pub const StdoutExporter = @import("metrics/exporters/stdout.zig").StdoutExporter;
+pub const OTLPExporter = @import("metrics/exporters/otlp.zig").OTLPExporter;
