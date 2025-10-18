aa917bedeb800dc205ab558d8c89f2215752022b
kmos <kmos@commonhaus.dev>
2025-10-18 13:32:10 +0200

    logs(sdk): move Logger and Logger provider in API
    Signed-off-by: inge4pres <fgualazzi@gmail.com>

--- src/sdk/logs.zig
+++ src/sdk/logs.zig
@@ -1,9 +1,5 @@
 //! OpenTelemetry Logs SDK.
 
-// SDK Components
-pub const LoggerProvider = @import("logs/provider.zig").LoggerProvider;
-pub const Logger = @import("logs/provider.zig").Logger;
-
 // Processors
 pub const LogRecordProcessor = @import("logs/log_record_processor.zig").LogRecordProcessor;
 pub const SimpleLogRecordProcessor = @import("logs/log_record_processor.zig").SimpleLogRecordProcessor;
@@ -15,7 +11,6 @@ pub const StdoutExporter = @import("logs/exporters/generic.zig").StdoutExporter;
 pub const InMemoryExporter = @import("logs/exporters/generic.zig").InMemoryExporter;
 
 test {
-    _ = @import("logs/provider.zig");
     _ = @import("logs/log_record_processor.zig");
     _ = @import("logs/log_record_exporter.zig");
     _ = @import("logs/exporters/generic.zig");
