90bd334ecc608693b41f8b0f77c285e90c9c3e35
inge4pres <fgualazzi@gmail.com>
2026-07-24 15:45:17 +0200

    modules: new openteletry-proto module
    Signed-off-by: inge4pres <fgualazzi@gmail.com>

---
+++ opentelemetry-proto/src/root.zig
@@ -0,0 +1,12 @@
+pub const common_v1 = @import("./opentelemetry/proto/common/v1.pb.zig");
+pub const resource_v1 = @import("./opentelemetry/proto/resource/v1.pb.zig");
+
+pub const collector_logs_v1 = @import("./opentelemetry/proto/collector/logs/v1.pb.zig");
+pub const collector_metrics_v1 = @import("./opentelemetry/proto/collector/metrics/v1.pb.zig");
+pub const collector_trace_v1 = @import("./opentelemetry/proto/collector/trace/v1.pb.zig");
+
+pub const logs_v1 = @import("./opentelemetry/proto/logs/v1.pb.zig");
+pub const metrics_v1 = @import("./opentelemetry/proto/metrics/v1.pb.zig");
+pub const trace_v1 = @import("./opentelemetry/proto/trace/v1.pb.zig");
+
+pub const profiles_v1development = @import("./opentelemetry/proto/profiles/v1development.pb.zig");
